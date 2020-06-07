7465285371713ba60775cfdda13fe0d81c62717e
Anus079 <anuszahid0798@gmail.com>
2020-06-07 15:44:11 +0500

    javascript Assignment

---
+++ app.js
@@ -0,0 +1,184 @@
+        //.... Chapter-1 Code....
+
+// serial# 1 Code..xxx
+
+    // alert("Hello world")
+
+// Serial # 2 Code...xx
+
+    // alert("Error! Please enter a valid password")
+
+// Serial # 3 code...
+
+    //  alert("Welcome to JS Land\nHappy coding !")
+
+// Serial # 4 code
+
+    // alert("Welcome to JS Land")
+    // alert("Happy Coding!\nprevent this page from creating addtional dialogs")
+
+// Serial # 5 code
+
+    // console.log("Hello... I can Run JS Through my web browsers console")    
+
+
+// .........Serial # 6
+
+    // alert("This is my first Javascipt Assignemt")
+
+
+
+    // ....Chapter 2 code of Javascript..
+
+// ....Serial # 1
+
+    // var username
+
+// Serial # 2
+
+    // var myname = "Anus Zahid"
+
+// Serial # 3
+
+    // var message = "Hello World"
+    // alert(message)
+
+// Serial # 4
+
+    // var Name = "Jhon Doe"
+    // var age = "15 years old"
+    // var enrol = "Certified Mobile Application develpoment"
+    // alert(Name)
+    // alert(age)
+    // alert(enrol)
+
+// Serial # 5.
+
+    // var one = "PIZZA";
+    // one = "PIZZ";
+    // one = "PIZ";
+    // one = "PI";
+    // one = "P";
+    // alert(one)
+
+//  Serail # 6..
+
+    // var address = "My Email Address is"
+    // var email = " Anaszahid079@gmail.com"
+    // var print = address.concat(email)
+    // alert(print)
+
+// Serial # 7..
+
+    // var message = "i am trying to learn from the book "
+    // var book = " A smarter way to learn JavaScript"
+    // var print = message.concat(book)
+    // alert(print)
+
+//  Serail #8 ..
+
+    // document.write(" <h1> Yah! I can Wrtie HTML Content through Javascript </h1>")
+
+//  serial # 9..
+
+    // var design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+   
+    // alert(design)
+
+
+
+         // ..CHAPTER # 3 Java script Assignment......
+
+//  Serial # 1
+
+    //  var age = "I am 20 years old"
+    //  alert (age)
+
+//  Serail # 2
+
+    //  var visit = ("you hav vistied this site one time")
+    //  alert(visit)
+
+//  Serial # 3
+        
+        // var birthyear = " <h1>my birth year is 2000</h1>"; 
+        // document.write(birthyear)
+
+//  Serial # 4
+
+    //  var visitorname = "jhon doe";
+    //  var producttile = " T-shirt(s)"
+    //  var quantity = 5;
+    //  document.write(visitorname+" orderd "+quantity+producttile+" on XYZ clothing store")
+
+
+    // ..Chapter # 4 Java script Assignemt...
+
+// Serial # 1
+
+    // var a = "anas";var b = "areeb";var c = "haris";
+
+
+// Serial # 2
+
+//   First five Legal variable name...
+        // var _ = 2;
+        // var name = "Anas";
+        // var $ = "Dollar";
+        // var \u006f = "legal";
+        // var useralert = "alert";
+
+//  Second Five illegal variable name..
+
+        // var first name = name;
+        // var 1 = one;
+        // var & = "and";
+        // var * = "Multiply";
+        // var ! = "illegal";
+
+// Serial # 3..
+
+        // var name = "<h1>Rules for naming JS Variables</h1>"
+        // var legal = "vaiable name can only contain,Numbers,$,_,javascript keywordlike user alert, and capital words like Pakistan canbe variable"
+
+
+
+
+
+        // .. Chapter #5....
+// Serial # 3..
+
+// var a = 5;
+// var b = 6;
+// var c = ++;
+
+// Serial #4
+
+// var a = 600;
+// var b = 5;
+// var c = (a* b);
+// alert( c)
+
+//  serial # 5
+
+// CHAPTER5 9-11
+
+//  Serial # 2
+
+// var gender = prompt("enter your gender")
+
+//  var gender = prompt("Enter your gender?");
+//  if (gender === "male") {
+//  alert("Good morning sir");
+//  }
+//  if (gender === "female") {
+//     alert("Good morning Mamm!");
+//     }
+
+// Chapter 17-20
+
+// serial # 3
+
+
+
+
