44075fa0e3452409c9654a6eda015a4a034f2569
Anus079 <anuszahid0798@gmail.com>
2020-06-14 16:56:18 +0500

    javascript Assignment

--- app.js
+++ app.js
@@ -1,151 +1,151 @@
-        //.... Chapter-1 Code....
+//.... Chapter-1 Code....
 
 // serial# 1 Code..xxx
 
-    // alert("Hello world")
+// alert("Hello world")
 
 // Serial # 2 Code...xx
 
-    // alert("Error! Please enter a valid password")
+// alert("Error! Please enter a valid password")
 
 // Serial # 3 code...
 
-    //  alert("Welcome to JS Land\nHappy coding !")
+//  alert("Welcome to JS Land\nHappy coding !")
 
 // Serial # 4 code
 
-    // alert("Welcome to JS Land")
-    // alert("Happy Coding!\nprevent this page from creating addtional dialogs")
+// alert("Welcome to JS Land")
+// alert("Happy Coding!\nprevent this page from creating addtional dialogs")
 
 // Serial # 5 code
 
-    // console.log("Hello... I can Run JS Through my web browsers console")    
+// console.log("Hello... I can Run JS Through my web browsers console")    
 
 
 // .........Serial # 6
 
-    // alert("This is my first Javascipt Assignemt")
+// alert("This is my first Javascipt Assignemt")
 
 
 
-    // ....Chapter 2 code of Javascript..
+// ....Chapter 2 code of Javascript..
 
 // ....Serial # 1
 
-    // var username
+// var username
 
 // Serial # 2
 
-    // var myname = "Anus Zahid"
+// var myname = "Anus Zahid"
 
 // Serial # 3
 
-    // var message = "Hello World"
-    // alert(message)
+// var message = "Hello World"
+// alert(message)
 
 // Serial # 4
 
-    // var Name = "Jhon Doe"
-    // var age = "15 years old"
-    // var enrol = "Certified Mobile Application develpoment"
-    // alert(Name)
-    // alert(age)
-    // alert(enrol)
+// var Name = "Jhon Doe"
+// var age = "15 years old"
+// var enrol = "Certified Mobile Application develpoment"
+// alert(Name)
+// alert(age)
+// alert(enrol)
 
 // Serial # 5.
 
-    // var one = "PIZZA";
-    // one = "PIZZ";
-    // one = "PIZ";
-    // one = "PI";
-    // one = "P";
-    // alert(one)
+// var one = "PIZZA";
+// one = "PIZZ";
+// one = "PIZ";
+// one = "PI";
+// one = "P";
+// alert(one)
 
 //  Serail # 6..
 
-    // var address = "My Email Address is"
-    // var email = " Anaszahid079@gmail.com"
-    // var print = address.concat(email)
-    // alert(print)
+// var address = "My Email Address is"
+// var email = " Anaszahid079@gmail.com"
+// var print = address.concat(email)
+// alert(print)
 
 // Serial # 7..
 
-    // var message = "i am trying to learn from the book "
-    // var book = " A smarter way to learn JavaScript"
-    // var print = message.concat(book)
-    // alert(print)
+// var message = "i am trying to learn from the book "
+// var book = " A smarter way to learn JavaScript"
+// var print = message.concat(book)
+// alert(print)
 
 //  Serail #8 ..
 
-    // document.write(" <h1> Yah! I can Wrtie HTML Content through Javascript </h1>")
+// document.write(" <h1> Yah! I can Wrtie HTML Content through Javascript </h1>")
 
 //  serial # 9..
 
-    // var design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
-   
-    // alert(design)
+// var design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// alert(design)
 
 
 
-         // ..CHAPTER # 3 Java script Assignment......
+// ..CHAPTER # 3 Java script Assignment......
 
 //  Serial # 1
 
-    //  var age = "I am 20 years old"
-    //  alert (age)
+//  var age = "I am 20 years old"
+//  alert (age)
 
 //  Serail # 2
 
-    //  var visit = ("you hav vistied this site one time")
-    //  alert(visit)
+//  var visit = ("you hav vistied this site one time")
+//  alert(visit)
 
 //  Serial # 3
-        
-        // var birthyear = " <h1>my birth year is 2000</h1>"; 
-        // document.write(birthyear)
+
+// var birthyear = " <h1>my birth year is 2000</h1>"; 
+// document.write(birthyear)
 
 //  Serial # 4
 
-    //  var visitorname = "jhon doe";
-    //  var producttile = " T-shirt(s)"
-    //  var quantity = 5;
-    //  document.write(visitorname+" orderd "+quantity+producttile+" on XYZ clothing store")
+//  var visitorname = "jhon doe";
+//  var producttile = " T-shirt(s)"
+//  var quantity = 5;
+//  document.write(visitorname+" orderd "+quantity+producttile+" on XYZ clothing store")
 
 
-    // ..Chapter # 4 Java script Assignemt...
+// ..Chapter # 4 Java script Assignemt...
 
 // Serial # 1
 
-    // var a = "anas";var b = "areeb";var c = "haris";
+// var a = "anas";var b = "areeb";var c = "haris";
 
 
 // Serial # 2
 
 //   First five Legal variable name...
-        // var _ = 2;
-        // var name = "Anas";
-        // var $ = "Dollar";
-        // var \u006f = "legal";
-        // var useralert = "alert";
+// var _ = 2;
+// var name = "Anas";
+// var $ = "Dollar";
+// var \u006f = "legal";
+// var useralert = "alert";
 
 //  Second Five illegal variable name..
 
-        // var first name = name;
-        // var 1 = one;
-        // var & = "and";
-        // var * = "Multiply";
-        // var ! = "illegal";
+// var first name = name;
+// var 1 = one;
+// var & = "and";
+// var * = "Multiply";
+// var ! = "illegal";
 
 // Serial # 3..
 
-        // var name = "<h1>Rules for naming JS Variables</h1>"
-        // var legal = "vaiable name can only contain,Numbers,$,_,javascript keywordlike user alert, and capital words like Pakistan canbe variable"
+// var name = "<h1>Rules for naming JS Variables</h1>"
+// var legal = "vaiable name can only contain,Numbers,$,_,javascript keywordlike user alert, and capital words like Pakistan canbe variable"
 
 
 
 
 
-        // .. Chapter #5....
+// .. Chapter #5....
 // Serial # 3..
 
 // var a = 5;
@@ -175,10 +175,269 @@
 //     alert("Good morning Mamm!");
 //     }
 
+//  Chapter 12-13 !
+
+//  Task # 1....
+
+// var value = prompt( "Enter Your Name");
+// var check = value;
+// if ( check === check.toUpperCase()) {
+//     alert ( "This is Upper case letter")
+// }
+// else if( value === check.toLowerCase()) {
+//     alert ("This is lower case letter")
+// }
+
+//  Task # 2...
+
+// var num1 = prompt("Enter your first value");
+// var num2 = prompt("Enter your second value");
+
+// if (  num1>num2){
+//     alert ("your fisrt value is larger value")
+// }
+// else if ( num2>num1)(
+//     alert ("your second value is larger value")
+// )
+// else if ( num1 === num2 || num2 === num1)(
+//     alert("your value is equal try again")
+// )
+
+
+//  Task # 3...
+
+
+//  var num = prompt( " Enter your number");
+//  if (num>0){
+//      alert(  "Your number is posstive")
+//  }
+//  else if (num<0){
+//      alert("Your number is negative")
+//  }
+//  else if (num == 0){
+//      alert ("Your number is zero")
+//  }
+
+
+//  Task # 4....
+
+// var val = prompt( "Enter vowel character")
+// if (val == "a"){
+//     alert( "true")
+// }
+// else if (val == "e"){
+//     alert( "true")
+// }
+// else if (val == "i"){
+//     alert( "true")
+// }
+// else if (val == "o"){
+//     alert( "true")
+// }
+// else if (val == "u"){
+//     alert( "true")
+// }
+// else{
+//     alert ("false")
+// }
+
+//  Task # 5 ...
+
+// var pass = 123;
+// var ask = prompt(" Enter your password")
+// if ( ask === ""){
+//     alert( "please Enter your password")
+// }
+// else if ( ask == pass){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else{
+//     alert("Incorrect password")
+// }
+
+
+//  Task # 6 ...
+
+    // var greeting;
+    // var hour = 13;
+    // if (hour < 18) {
+    // greeting = "Good day";}
+    // else{
+    //     greeting = "Good evening";
+    // }
+    // alert(greeting)
+
+
+
+//  Task # 7...
+
+
+// var time = 1900;
+// if(time >=0000 && time <=1200  ){
+//     alert( "Good morning")
+// }
+// else if ( time >= 1200 && time <= 1700){
+//     alert("Good afternon!")
+// }
+// else if ( time >= 1700 && time <= 2100){
+//     alert("Good Evening!")
+// }
+// else if ( time >= 2100 && time <= 2359){
+//     alert("Good Night")
+// }
+
+
+        //  Chapter 14-16 ...
+
+// Task # 1 ....
+
+    // var Studentsname = [];
+    
+//  Task # 2...
+
+    // var studentsname = [];
+
+//  Task # 3... 
+
+    // var studentsname = ["Anas","zahid","subhan","haris"];
+
+//  Task # 4...
+
+    // var number = [1,2,3,4,5,6,7];
+
+// Task # 5..
+
+    // var cityname = ["karachi","lahore","fasialabad","peshawar"];
+    // var ans = prompt( "Enter your city");
+    //     if ( ans === cityname[0]){
+    //         alert(true)
+            
+    //     }
+    //     else if( ans =="" || ans == " "){
+    //         alert("Please enter your city")
+    //     }
+    //     else{
+    //         alert(false)
+            
+    //     }
+        
+// Task # 6..
+
+    // var array = ["Anas","zahid",1,2,3,];
+
+// Task # 7 ... Isey krna ha 
+
+    // var qualification = ["1) SSC" + " 2) HSC" + " 3) BSC" + " 4) BS" + " 5) BCOM" +  "6) MS" + " 7) M.PHIL." + " 8) PhD"];
+    // document.write(qualification)
+    
+//  Task # 8 ... 
+
+
+        // var studentsname = ["Anus","Fahad","Subhan"];
+        // var Score = [320,230,480];
+        // var totalmarks = [500,500,500];
+        // alert("Score of " + studentsname[1] + " is " + Score[1] + "." + "Percentage" + ":" + " " + Score[1]/totalmarks[1]*100 + "%" )
+        
+
+
+
+//  ...Task # 9
+
+        //  Task # (A)
+
+        // var colornames = ["orange","red","pink"];
+        // colornames.unshift(prompt("what color do you want"))
+        // document.write(colornames)
+
+        // Task # (B)
+        
+        // var colornames = ["orange","red","pink"];
+        // colornames.push(prompt("what color do you want"))
+        // document.write(colornames)
+        
+        
+        // Task # (C)
+        
+        // var colornames = ["orange","red","pink"];
+        // colornames.unshift(prompt("Enrer your first color"));
+        // colornames.unshift(prompt("Enrer your second color "));
+        // document.write(colornames)
+        
+        
+        // Task # (D)
+        
+        // var colornames = ["orange","red","pink"];
+        // colornames.shift()
+        // document.write(colornames)
+        
+        
+        // Task # (E)
+        
+        // var colornames = ["orange","red","pink"];
+        // colornames.pop()
+        // document.write(colornames)
+        
+        
+        // Task # (F)
+        
+        
+        // var colornames = ["orange","red","pink","yellow","blue"];
+        // colornames.splice(prompt("Which index do you want add a Color") ,0 ,prompt("which color do you want add"));
+        // document.write(colornames)
+        
+        
+        
+        // T ask # (G)
+        
+        
+        // var colornames = ["orange","red","pink","yellow","blue"];
+        // colornames.splice( prompt("Which index do you want delete a Color"), prompt("How many colors you want to delete"));
+        // document.write(colornames)
+        
+
+
+//  ...Task # 10
+
+        // var studentsscore = [320,230,480,120]
+        // studentsscore.sort();
+        // document.write(studentsscore)
+
+
+//  Task # 11...
+
+    // var citynames = ["karachi","lahore","Islamabad","Quetta","Peshawar"];
+    // var selected = citynames.slice(2,4)
+    // document.write(selected) 
+
+
+
+// Task # 12...
+
+    // var arr = ["This","is","My","Car"]
+    // alert(arr.join(" "))
+
+// Task# 13...
+
+    // var storevalues = ["Keyboard","mouse","printer","monitor"];
+    // var fifo = storevalues;
+    // console.log(storevalues)
+   
+    
+// Task #14...
+
+    // var storevalues = ["Keyboard","mouse","printer","monitor"];
+    // var lifo = storevalues.reverse();
+    // console.log(storevalues)
+
+
+
+// Task # 15...
+
+    // var phone = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
+    // document.write(phone)
+
 // Chapter 17-20
 
 // serial # 3
-
-
 
 
